4c6e0330ff42d0706e9403d6f611bb1886ffa5ce
Bader533 <bader.hani11@gmail.com>
2023-08-29 12:02:27 +0300

    fix search function in front page

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -34,10 +34,12 @@ class OperationController extends Controller
         $endDate = date("Y-m-d", strtotime($dates[1]));
 
         $avalibleLounges = Lounge::whereDoesntHave('bookings', function ($query) use ($startDate, $endDate) {
-            $query->where(function ($query) use ($startDate, $endDate) {
-                $query->whereBetween('start_date', [$startDate, $endDate])
-                    ->orWhereBetween('end_date', [$startDate, $endDate]);
-            });
+            $query->whereBetween('start_date', [$startDate, $endDate])
+                ->orWhereBetween('end_date', [$startDate, $endDate])
+                ->orWhere(function ($query) use ($startDate, $endDate) {
+                    $query->where('start_date', '<', $startDate)
+                        ->where('end_date', '>', $endDate);
+                });
         })->get();
 
         return view('frontend.lounges', ['avalibleLounges' => $avalibleLounges, 'date' => $request->date]);
